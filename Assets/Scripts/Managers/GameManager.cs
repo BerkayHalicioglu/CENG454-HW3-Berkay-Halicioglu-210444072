e180c69c711a94ae36ccd0a3f499b3227cacea34
BerkayHalicioglu <haliciogluberkay@gmail.com>
2026-05-10 22:24:51 +0300

    feat: add GameManager with win/lose handling

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameEvents.Instance.OnGameOver += HandleGameOver;
+        GameEvents.Instance.OnCoreDied += HandleCoreDied;
+    }
+
+    private void OnDisable()
+    {
+        if (GameEvents.Instance == null) return;
+        GameEvents.Instance.OnGameOver -= HandleGameOver;
+        GameEvents.Instance.OnCoreDied -= HandleCoreDied;
+    }
+
+    private void HandleGameOver(bool playerWon)
+    {
+        if (playerWon)
+            Debug.Log("KAZANDIN! Tüm dalgalar temizlendi.");
+        else
+            Debug.Log("KAYBETTİN! Core yok edildi.");
+
+        Invoke(nameof(ReloadScene), 3f);
+    }
+
+    private void HandleCoreDied()
+    {
+        GameEvents.Instance.GameOver(false);
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
